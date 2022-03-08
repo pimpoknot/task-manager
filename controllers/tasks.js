0736d34f9315c66c226cc45a91dcc0a82e1200a7
Marcos Maverick <marquinhopimpo@gmail.com>
2022-03-07 22:14:34 -0300

    validation of method createTask

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,20 +1,42 @@
 const Task = require('../models/Task')
 
-const getAllTasks = (req, res) => {
-    res.send('get all tasks')
+const getAllTasks = async (req, res) => {
+   try  {
+        const tasks = await Task.find({})
+        res.status(200).json({ tasks })
+   } catch (err) {
+        res.status(500).json({msg: err})
+   }
 }
 
 const createTasks = async (req, res) => {
     const { name, completed } = req.body
-    const task = await Task.create({
-        name:name,
-        completed:completed
-    })
-    res.status(201).json({task})
+    try {
+        const task = await Task.create({
+            name: name,
+            completed: completed
+        })
+        res.status(201).json({ task })
+    } catch (err) {
+        res.status(500).json({ msg: err })
+    }
+
+    
 }
 
-const getTasks = (req, res) => {
-    res.json({id: req.params.id})
+const getTasks = async (req, res) => {
+    try {
+        const { id:taskID } = req.params
+        const task = await Task.findOne({__id:taskID})
+        if(!task) {
+            return res.status(404).json({msg: `No task with id: ${taskID}`})
+        }
+
+        res.status(200).json({ task })
+    } catch (err) {
+        res.status(500).json({ msg: err })
+    }
+   
 }
 const updateTasks = (req, res) => {
     res.send('update task')
